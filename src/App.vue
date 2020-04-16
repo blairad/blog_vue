317c239ee6b182d8eee6ec75e1639772141fc7bb
Andrew Blair <blair.d.andrew@gmail.com>
2020-04-16 15:15:29 +0100

    first commit. set up blog via youtube netninja

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div >
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+
+import addBlog from './components/addBlog'
+
+export default {
+  components: {
+    'add-blog': addBlog
+  },
+ 
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: 'nunito SemiBold';
+}
+
+</style>
